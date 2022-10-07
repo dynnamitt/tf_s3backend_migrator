4d80805b76afc6ce5b151f433e0a6bd5576f9adf
kjetil midtlie <kjetil.midtlie@gmail.com>
2022-10-07 21:53:17 +0200

    build if no .so

--- tf_s3backend_migration/tree_sitter_utils/parsers.py
+++ tf_s3backend_migration/tree_sitter_utils/parsers.py
@@ -1,5 +1,6 @@
 from tree_sitter import Language, Parser
 from pathlib import Path
+from typing import List
 import sys
 
 OUT_LIB = 'build/my_langs.so'
@@ -8,22 +9,28 @@ TS_PARSERDIR_PRE = 'tree-sitter-'
 class Parsers:
 
     def __init__(self,top_path:Path):
-        print(str(top_path))
         self._parser_dirs = [p for p in top_path.iterdir() 
             if p.is_dir() and p.name.startswith(TS_PARSERDIR_PRE)]
         self._lang_names = [d.name.removeprefix(TS_PARSERDIR_PRE) for d in self._parser_dirs]
-        print("names",self._lang_names)
+        try:
+            self._init_langs()
+        except OSError as e:
+            print(e)
+            print("WARN: Failed to find libs w/ languages, will try to build ...")
+            self._build()
+            self._init_langs()
+
+    def _init_langs(self):
         self._langs = { n : Language(OUT_LIB,n) for n in self._lang_names}
 
-    #try:
-    #    Language.build_library(OUT_LIB,[f"{top_path}/{TS_PARSERDIR_PREFIX}{lib}" for lib in LIBS])
-    #except FileNotFoundError as e:
-    #    print(e)
-    #    print(f"ERROR: Didn't you DOWNLOAD 'tree-sitter-{LIBS}' to {top_path} ?")
-    #    sys.exit(1)
+    def _build(self):
+        try:
+            Language.build_library(OUT_LIB,self._parser_dirs)
+        except FileNotFoundError as e:
+            print(e)
+            sys.exit(1)
     
     def get_parser(self,lang):
-        print(self._lang_names)
         p = Parser()
         p.set_language(self._langs.get(lang))
         return p
