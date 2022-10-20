b908d1e2d7da6242849422ac358062ae21b384cd
kjetil midtlie <kjetil.midtlie@gmail.com>
2022-10-21 00:17:30 +0200

    collecting datafiles for gen1 and gen2

--- tests/test_psudo_wrkspc.py
+++ tests/test_psudo_wrkspc.py
@@ -6,15 +6,21 @@ import tf_s3backend_migrator.psudo_workspaces as sut
 
 HERE = Path(__file__).absolute().parent
 
-class TestPWorkspaces(unittest.TestCase):
-
+class TestPsudoWorkspaces(unittest.TestCase):
     def test_gen1(self):
+
         dir = Path(HERE,"fixture/gen1")
-        match_pattern = "%/init.tfvars"
+        match_pattern = "%/input.tfvars"
         tf_wrkspcs1 = sut.scan_tf_dir(dir, match_pattern, ["tf-code"])
         self.assertEqual(len(tf_wrkspcs1),3)
+
+        for w in tf_wrkspcs1:
+            w.append_init("init.tfvars")
+
         pprint(tf_wrkspcs1)
 
+    def test_gen1_nomatch(self):
+        dir = Path(HERE,"fixture/gen1")
         match_pattern2 = "input-%.tfvars"
         tf_wrkspcs2 = sut.scan_tf_dir(dir, match_pattern2, ["tf-code"])
         self.assertEqual(len(tf_wrkspcs2),0)
@@ -24,6 +30,8 @@ class TestPWorkspaces(unittest.TestCase):
         match_pattern = "input-%.tfvars"
         tf_wrkspcs1 = sut.scan_tf_dir(dir, match_pattern, ["tf-code"])
         self.assertEqual(len(tf_wrkspcs1),6)
+        for w in tf_wrkspcs1:
+            w.append_init("Makefile")
         pprint(tf_wrkspcs1)
 
 if __name__ == '__main__':
--- tf_s3backend_migrator/psudo_workspaces.py
+++ tf_s3backend_migrator/psudo_workspaces.py
@@ -1,39 +1,46 @@
 # analyze dirs
-from typing import List
+from typing import List,Optional
 from pathlib import Path
 import glob
 import re
 from dataclasses import dataclass
+from pprint import pprint
 
 WILDCARD = '%'
 NON_SLASH_GRP_MATCH = '([^/]*)'
 
 @dataclass
-class TerraformPsudoWorkSpace:
+class TFWannabeWorkSpace:
     name: str
     input_file: Path
+    init_file: Optional[Path]
 
     @classmethod
-    def from_match(cls,match_pattern:str,input_file:Path) -> 'TerraformPsudoWorkSpace':
+    def from_match(cls,match_pattern:str,input_file:Path) -> 'TFWannabeWorkSpace':
         regex_ = match_pattern.replace(WILDCARD, NON_SLASH_GRP_MATCH)
         f = str(input_file.absolute())
         s_result = re.search(regex_, f)
         name = s_result.group(1)
-        return TerraformPsudoWorkSpace(name,input_file)
+        return TFWannabeWorkSpace(name,input_file,None)
+
+    def append_init(self,glob_pattern:str):
+        assert self.input_file.exists()
+        results = list(self.input_file.parent.glob(glob_pattern))
+        assert len(results) == 1 , "Expected ONE match"
+        self.init_file = results[0]
+        
 
 def scan_tf_dir(dir:Path,
-                    match_pattern:str,
-                    excludes:List[str] = []) -> List[TerraformPsudoWorkSpace]:
+                    inputs_pattern:str,
+                    excludes:List[str] = []) -> List[TFWannabeWorkSpace]:
     # first check for local patterns
+    assert dir.is_dir()
+    assert WILDCARD in inputs_pattern, f"Must have a single '{WILDCARD}' in match_pattern!"
 
-    assert WILDCARD in match_pattern, f"Must have a single '{WILDCARD}' in match_pattern!"
-
-    g_pattern = match_pattern.replace(WILDCARD,"*").lstrip("/")
+    g_pattern = inputs_pattern.replace(WILDCARD,"*").lstrip("/")
     cand_files = [Path(f) for f in glob.glob(str(dir) + "/" + g_pattern)]
     matching_files = [f for f in cand_files if f.name not in excludes]
-    return [TerraformPsudoWorkSpace.from_match(match_pattern,f) for f in matching_files]
-
-
+    return [TFWannabeWorkSpace.from_match(inputs_pattern,f) for f in matching_files]
 
 
 
