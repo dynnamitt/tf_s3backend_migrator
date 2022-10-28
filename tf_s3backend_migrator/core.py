f031fb889d71b2119d2b2631d3377cfce0006488
kjetil midtlie <kjetil.midtlie@gmail.com>
2022-10-28 10:42:18 +0200

    refact

--- tf_s3backend_migrator/core.py
+++ tf_s3backend_migrator/core.py
@@ -1,24 +1,17 @@
+from dataclasses import dataclass
 from functools import lru_cache
 from . import psudo_workspaces as pw
 from . import queries as q
 from . import aws
 from rich import print
 from pathlib import Path
-from typing import List,Dict
+from typing import List, NamedTuple
 import click
 
-MK_FILE = "Makefile"
-
-pattern_pairs = {
-        ("%/input.tfvars","init.tfvars"),
-        ("input-%.tfvars",MK_FILE)
-        }
-
-TF_CODE_DIR = "tf-code"
 
 DEF_ARN = "arn:aws:iam::{}:role/admin"
 
-# 100% perf : make enum
+# make enum ?
 EXPECTED_VALS = {"bucket",
                  "region",
                  "dynamodb_table",
@@ -27,10 +20,14 @@ EXPECTED_VALS = {"bucket",
 
 CS = ["cyan","blue","yellow","red","orange","magenta"]
 
-def main(root_dir:Path,new_backend_tf:Path):
+@dataclass()
+class StateBackup:
+    workspace_name:str
+    temp_file:Path
+
+def main(root_dir:Path, new_backend_tf:Path):
     """Select the type and handle it"""
 
-    assert new_backend_tf.suffix == ".tf"
     try:
         dest_backend_keys = q.parse_file(new_backend_tf).tf_backend_body_kv()
     except q.TSQueryError as e:
@@ -42,30 +39,27 @@ def main(root_dir:Path,new_backend_tf:Path):
     print("Warning: Missing keys:",diff_new_backend_tf) if len(diff_new_backend_tf)>0 else None
     if not click.confirm("Continue ?"):
         return
-
-    for patt,init_glob in pattern_pairs:
-        ws = pw.scan_tf_dir(root_dir,patt,excludes=[TF_CODE_DIR])
     
-        ws_names = [w.name for w in ws] 
-        if len(ws)>0 :
-            print(f"\n:bulb: Project_dir is of type '{patt}'\n")
-            print(f"Psudo-workspaces located:")
-            [print(f" - [{CS[idx+1]}]{w}[/{CS[idx+1]}]") for idx,w in enumerate(ws_names)]
-            handle_current(root_dir,init_glob,ws)
+    project = pw.get_legacy_project(root_dir)
 
-                
-def handle_current(root_dir :Path,
-                     init_glob :str,
-                     ws: List[pw.TFWannabeWorkSpace]):
+    print(f"\n:bulb: Project_dir is of type '{project.pattern}'\n")
+    print(f"Psudo-workspaces located:")
+    ws_names = [w.name for w in project.workspaces] 
+    for idx,w in enumerate(ws_names):
+        print(f" - [{CS[idx+1]}]{w}[/{CS[idx+1]}]") 
+     
+    state_backups = handle_downloads( Path(root_dir,pw.TF_CODE_DIR),project)
 
-    code_path = Path(root_dir,TF_CODE_DIR)
-    for idx,w in enumerate(ws):
-        w.append_init(init_glob)
+
+
+def handle_downloads(code_path :Path, project:pw.LegacyProject) -> List[StateBackup]: 
+    backups = []
+    for idx,w in enumerate(project.workspaces):
         print()
         print(f"====== Psudo-TF-Workspace: [{CS[idx+1]}]{w.name}[/{CS[idx+1]}] :computer:")
         print()
         init_result = q.parse_file(w.init_file)
-        init_vals = init_result.tf_backend_body_kv() if w.init_file.name == MK_FILE else init_result.key_values()
+        init_vals = init_result.tf_backend_body_kv() if w.init_file.name == pw.MK_FILE else init_result.key_values()
         vars = q.parse_file(w.input_file).key_values()
         # pprint(vars)
         diff1 = EXPECTED_VALS.difference(init_vals.keys())
@@ -81,7 +75,7 @@ def handle_current(root_dir :Path,
         diff2 = EXPECTED_VALS.difference(init_vals.keys())
         if len(diff2) == 1 and "role_arn" in diff2:
             acc_num = vars.get('account',init_vals.get('account',"???????????"))
-            ask = "'role_arn' isn't specified in the code, please Enter"
+            ask = "\n- 'role_arn' isn't specified in the code\n  please Enter"
             r = click.prompt(text=ask, default = DEF_ARN.format(acc_num))
             init_vals["role_arn"] = r
         elif len(diff2) > 2:
@@ -91,10 +85,13 @@ def handle_current(root_dir :Path,
         # return init_vals
         print(init_vals)
         print("Downloading ...")
-        aws.download_s3_obj("tf_migr_tool",**init_vals)
-        # oh god ... clean up these for-loops eh??
-        
-                
+        temp_file = aws.download_s3_obj("tf_migr_tool",**init_vals)
+        backups.append(StateBackup(w.name,temp_file))
+
+    # <-- indent 
+    # oh god ... clean up these for-loops eh??
+    return backups
+                        
 @lru_cache()
 def scan_dir_backend_kvs(code_dir:Path) -> q.QResult:
     tfs = [f for f in code_dir.iterdir() if f.suffix == ".tf"]
--- tf_s3backend_migrator/psudo_workspaces.py
+++ tf_s3backend_migrator/psudo_workspaces.py
@@ -1,12 +1,15 @@
 # analyze dirs
-from typing import List,Optional
+from typing import List
 from pathlib import Path
 import glob
 import re
 from dataclasses import dataclass
 
+MK_FILE = "Makefile"
+TF_CODE_DIR = "tf-code"
 WILDCARD = '%'
 NON_SLASH_GRP_MATCH = '([^/]*)'
+
 
 @dataclass
 class TFWannabeWorkSpace:
@@ -27,7 +30,6 @@ class TFWannabeWorkSpace:
         results = list(self.input_file.parent.glob(glob_pattern))
         assert len(results) == 1 , "Expected ONE match"
         self.init_file = results[0]
-        
 
 def scan_tf_dir(dir:Path,
                     inputs_pattern:str,
@@ -40,6 +42,28 @@ def scan_tf_dir(dir:Path,
     cand_files = [Path(f) for f in glob.glob(str(dir) + "/" + g_pattern)]
     matching_files = [f for f in cand_files if f.name not in excludes]
     return [TFWannabeWorkSpace.from_match(inputs_pattern,f) for f in matching_files]
+
+@dataclass()
+class LegacyProject:
+    pattern: str
+    init_file: str
+    def set_workspaces(self, ws:List[TFWannabeWorkSpace]):
+        self.workspaces = ws
+        for w in self.workspaces:
+            w.append_init(self.init_file)
+
+GEN1 = LegacyProject("%/input.tfvars", "init.tfvars")
+GEN2 = LegacyProject("input-%.tfvars", MK_FILE)   
+
+def get_legacy_project(root_dir:Path) -> LegacyProject:
+
+    for p in [GEN1,GEN2]:
+        ws_ = scan_tf_dir(root_dir, p.pattern, excludes=[TF_CODE_DIR])
+        p.set_workspaces(ws_)
+
+    # try?
+    p = [p for p in [GEN1,GEN2] if len(p.workspaces) > 0][0]
+    return p
 
 
 
